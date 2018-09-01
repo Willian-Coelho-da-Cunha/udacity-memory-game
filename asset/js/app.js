eef7f1635bae591edd53ae71218f30549b95487a
Willian Coelho da Cunha <willian_comp@outlook.com>
2018-09-01 05:09:02 -0300

    Remove 'card match' of div's if the game restart

--- asset/js/app.js
+++ asset/js/app.js
@@ -5,6 +5,18 @@ function startGame(){
 	game.gameOn = true;
 	game.gameOff = false;
 	game.click = 0;
+
+	/*------IF RESTART THE GAME: REMOVE CARD-MATCH CLASS------*/
+	let verifyClass = document.querySelector(".card-match");
+	if (!(verifyClass === null)) {
+		/*------REMOVE CARD-MATCH CLASS------*/
+		let cardMatrix = [];
+		cardMatrix = document.querySelectorAll(".card");
+		for (let i = 0; i < cardMatrix.length; i++) {
+			cardMatrix[i].textContent = "Willian";
+			cardMatrix[i].classList.remove("card-match");
+		}
+	}
 }
 
 function turnCard(event){}

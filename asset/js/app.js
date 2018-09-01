31d99142bdcd65dad3602f02b421fc3d9f248667
Willian Coelho da Cunha <willian_comp@outlook.com>
2018-09-01 18:06:01 -0300

    setting restart game function

--- asset/js/app.js
+++ asset/js/app.js
@@ -4,9 +4,15 @@ function startGame(){
 	/*------CHANGE THE GAME PROPERTIES------*/
 	game.gameOn = true;
 	game.gameOff = false;
-	game.click = 0;
 }
 
 function turnCard(event){}
 
 function delayDisplayCard(){}
+
+function restartGame(){
+	/*------CHANGE THE GAME PROPERTIES------*/
+	game.gameOn = false;
+	game.gameOff = true;
+	game.click = 0;
+}

e928b3c2e4b680cceb49d2c3a03748d007059536
Willian Coelho da Cunha <willian.cunha@sabium.com.br>
2020-04-15 00:14:31 -0300

    Update project.
    Remove jquery of the project.
    Remove bootstrap of the project.
    Improve style and interaction with the elements.

--- asset/js/app-head.js
+++ asset/js/app-head.js
@@ -1,297 +1,316 @@
 /**
-* @description Represents the time controllers.
-* @property {integer} hours - The value is the same as a digital clock.
-* @property {integer} minutes - The value is the same as a digital clock.
-* @property {integer} seconds - The value is the same as a digital clock.
-* @property {string} time - This property receive the setTimeout method
+* @description Time controller.
+* @property { integer } hours Represents the number of hours.
+* @property { integer } minutes Represents the number of minutes.
+* @property { integer } seconds Represents the number of seconds.
+* @property { string } time This property receives the setTimeout method.
 */
+
 let timer = {
 	hours: 0,
 	minutes: 0,
 	seconds: 0,
-	time: "",
-	/**
-	* @description Start the digital timer
-	*/
-	timeOn(){
+	time: '',
+
+	/**@description Start counting the time.*/
+	timeOn() {
 		this.time = setTimeout(add, 983);
 	},
-	/**
-	* @description Stop the digital timer
-	*/
-	timeOff(){
+
+	/**@description Stop counting the time.*/
+	timeOff() {
 		clearTimeout(this.time);
 	},
-	/**
-	* @description Restart the timer when the user restarts the game.
-	*/
-	restartTimer(){
+
+	/**@description Restart the timer when the user restarts the game.*/
+	restartTimer() {
 		this.hours = 0;
 		this.minutes = 0;
 		this.seconds = 0;
-		const place = document.querySelector(".timer");
-		place.textContent = "00:00:00";
+		document.querySelector('.timer').textContent = '00:00:00';
 	}
 };
-/**
-* @description This function is responsable to encrease and display the current timer to the user
-*/
-function add(){
-	const objTimer = document.querySelector(".timer");
-	let clockString = "";
+
+/**@description This function is responsable to encreasing and displaying the current timer to the user.*/
+function add() {
+	let clockString = '';
 	
 	timer.seconds++;
-	if (timer.seconds > 59){
+
+	if (timer.seconds > 59) {
 		timer.seconds = 0;
 		timer.minutes++;
-		if (timer.minutes > 59){
+
+		if (timer.minutes > 59) {
 			timer.minutes = 0;
 			timer.hours++;
 		}
 	}
-	timer.hours < 10 ? clockString += "0" + timer.hours + ":" : clockString += timer.hours + ":";
-	timer.minutes < 10 ? clockString += "0" + timer.minutes + ":" : clockString += timer.minutes + ":";
-	timer.seconds < 10 ? clockString += "0" + timer.seconds : clockString += timer.seconds;
+	timer.hours < 10 ? clockString += '0' + timer.hours + ':' : clockString += timer.hours + ':';
+	timer.minutes < 10 ? clockString += '0' + timer.minutes + ':' : clockString += timer.minutes + ':';
+	timer.seconds < 10 ? clockString += '0' + timer.seconds : clockString += timer.seconds;
 	
-	objTimer.textContent = clockString;
+	document.querySelector('.timer').textContent = clockString;
 	timer.timeOn();
 }
 
 /**
-* @description This object has two methods. They are responsable to set and display a star rating to the user as a way 
-* to show his or her respective performance.
+* @description This object has two methods. They are responsable to setting and displaying a star rating to the user as a 
+* way to show his or her respective performance in the game.
 */
+
 let starRating = {
 	/**
-	* @description Restart the star rating when the user restarts the game.
-	* Use a 'document fragment' to prevent reflow and new design generation.
+	* @description Restart the star rating when the user restarts the game. Use a 'document fragment' to prevent reflow and 
+	* new design generation.
 	*/
-	restartStartRating(){
-		const rating = document.querySelector("#star-rating");
-		if(document.querySelector(".messageWithoutStars")){
-			let messageWithoutStars = document.querySelector(".messageWithoutStars");
-			messageWithoutStars.remove();
+	restartStartRating() {
+		let starRating = document.createDocumentFragment();
+		let stars = document.querySelectorAll('.star');
+
+		if (document.querySelector('.messageWithoutStars')) {
+			document.querySelector('.messageWithoutStars').remove();
 		}
-		let star = [];
-		star = document.querySelectorAll(".star");
-		let stars = document.createDocumentFragment();
-		/*posReview-1: Stars' number must be until 3*/
-		for (let i = star.length; i < 3; i++) {
-			let span = document.createElement("span");
-			span.setAttribute("class", "star");
-			span.innerHTML = "&#10023; ";
-			stars.appendChild(span);
-		}
-		rating.appendChild(stars);
-	},
-	/**
-	* @description When a specific number of moves happen, this function is called to remove a star from the user rating star.
-	*/
-	removeStar(){
-		let star = [];
-		star = document.querySelectorAll(".star");
-		star[star.length-1].remove();
-		if (star.length === 1){
-			let starRating = document.getElementById("star-rating");
-			let span = document.createElement("span");
-			span.setAttribute("class", "messageWithoutStars");
-			span.textContent = "Without stars!";
+
+		for (let i = stars.length; i < 3; i++) {
+			let span = document.createElement('span');
+			span.setAttribute('class', 'star');
+			span.innerHTML = '&#10023; ';
 			starRating.appendChild(span);
+		}
+
+		document.getElementById('star-rating').appendChild(starRating);
+	},
+
+	/**
+	* @description When a specific number of moves occur, this function is called to remove a star from the user's rating 
+	* star.
+	*/
+	removeStar() {
+		let star = document.querySelectorAll('.star');
+
+		star[star.length-1].remove();
+
+		if (star.length === 1) {
+			let span = document.createElement("span");
+
+			span.setAttribute('class', 'messageWithoutStars');
+			span.textContent = 'Without stars!';
+
+			document.getElementById('star-rating').appendChild(span);
 		}
 	}
 };
 
 /**
-* @description Capture the quantity of clickes done by the user.
-* @property {integer} moveCounter - This property is responsable to capture the clickes done by the user.
-*moveCounter value is not shown to the user.
-* @property {integer} displayCounter - A match is complete when two cards are the same. So, it is necessary two clicks to 
-*display the cards each time. Because of this, the displayCounter is increased once each two right clicks. Right clicks is 
-*the same as valid click. displayCounter value is shown to the user.
+* @description Capture the number of clicks made by the user.
+*
+* @property { integer } moveCounter This property is responsable for capturing the clicks made by the user. The value of 
+* 'moveCounter' property is not shown to the user.
+*
+* @property { integer } displayCounter One match occurs when two cards are equal. Therefore, two clicks are required to 
+* display the cards at a time. For this reason, the 'displayCounter' is increased once every two right clicks. Right 
+* clicks is the same as valid click. The value of 'displayCounter' property is shown to the user.
 */
 let moveCounter = {
 	moveCounter: 0,
 	displayCounter: 0,
+
 	/**
-	* @description Capture the valid click. If divisible by two, increase the displayCounter. And, it is responsable to 
-	*determine when a star will be removed from user star rating.
+	* @description Capture the valid click. If divisible by two, increase the value of 'displayCounter' property. And, it 
+	* is responsable for determining when a star will be removed from user's star rating.
 	*/
-	increaseCounter(){
+	increaseCounter() {
 		this.moveCounter++;
-		if ((this.moveCounter % 2) === 0){
+
+		if ((this.moveCounter % 2) === 0) {
 			this.displayCounter++;
 			this.showDisplayCounter();
 		}
+
 		if ((this.moveCounter === 48) || (this.moveCounter === 58) || (this.moveCounter === 68)) {
 			starRating.removeStar();
 		}
 	},
+
 	/**
-	* @description This method is called by increaseCounter method when the game needs to change the value of Move counter 
-	*on the Graphical User Interface.
+	* @description This method is called by 'increaseCounter' method when the game needs to change the value of 'Move 
+	* counter' on the Graphical User Interface.
 	*/
-	showDisplayCounter(){
-		const displayPlace = document.querySelector(".move-counter");
-		let show;
-		show = this.displayCounter <= 9 ? "0" + this.displayCounter : this.displayCounter;
-		displayPlace.textContent = show;
+	showDisplayCounter() {
+		if (this.displayCounter <= 9) {
+			document.querySelector('.move-counter').textContent = `0${String(this.displayCounter)}`;
+		} else {
+			document.querySelector('.move-counter').textContent = `${String(this.displayCounter)}`;
+		}
 	},
-	/**
-	* @description This method is responsable to restart the moveCounter parameters. As weel as, bring the move counter
-	*number to zero when the user restarts the game.
-	*/
-	restartCounter(){
-		const displayPlace = document.querySelector(".move-counter");
+
+	/**@description This method is responsable for restarting the 'moveCounter' parameters.*/
+	restartCounter() {
 		this.moveCounter = 0;
 		this.displayCounter = 0;
-		displayPlace.textContent = "00";
+		document.querySelector(".move-counter").textContent = '00';
 	}
 };
 
 /**
 * @description This the object game.
-* @property {integer} play - By default, the user plays the game once. But, if the user wants to play more times, this property 
-*will be know.
-* @property {integer} click - Counts how many clicks the user makes on a card. Avoid showing more than two cards each chance 
-of match.
+*
+* @property { integer } play By default, the user plays the game once. But, if the user wants to play more times, this 
+* property will be know.
+*
+* @property { integer } click - Counts how many clicks the user makes on a card. Avoid showing more than two cards each 
+* chance of match.
 */
+
 let game = {
 	play: 1,
 	click: 0,
-	/**
-	* @description End of the game.
-	*/
-	gameOver(){
+
+	/**@description End of the game.*/
+	gameOver() {
+		const node = document.getElementById('game-score');
+		let containerGameOver, chances, msn, stars, time;
+
+		let clockString = '';
+		let containers = [];
+		let starNodes = [];
+		let fragment = document.createDocumentFragment();
+
 		timer.timeOff();
 
-		/*------DISABLE THE EVENT------*/
-		let container = document.querySelector("#game-arena");
-		$(container).off('click', 'DIV');
-
-		const node = document.getElementById("game-score");
-		let fragment, containers, containerGameOver, chances, clockString, starNodes, msn, stars, time;
-		clockString = "";
-
-		containers = [];
-		if(document.querySelector(".containerGameOver")){
-			containers = document.querySelectorAll(".containerGameOver");
+		if (document.querySelector('.containerGameOver')) {
+			containers = document.querySelectorAll('.containerGameOver');
 		}
-		
-		fragment = document.createDocumentFragment();
-		
-		containerGameOver = document.createElement("div");
-		containerGameOver.setAttribute("class", "containerGameOver");
+
+		containerGameOver = document.createElement('div');
+		containerGameOver.setAttribute('class', 'containerGameOver');
 
 		/*------DISPLAY USER CHANCES OF WIN THE GAME------*/
-		chances = document.createElement("h6");
-		chances.setAttribute("class", "chanceNumber");
-		chances.textContent = "This was your attempt number " + this.play + ".";
+		chances = document.createElement('h6');
+		chances.setAttribute('class', 'chanceNumber');
+		chances.textContent = `This was your attempt number ${String(this.play)}.`;
 		containerGameOver.appendChild(chances);
 
 		/*------DISPLAY TIMER------*/
-		time = document.createElement("h5");
-			timer.hours < 10 ? clockString += "0" + timer.hours + ":" : clockString += timer.hours + ":";
-			timer.minutes < 10 ? clockString += "0" + timer.minutes + ":" : clockString += timer.minutes + ":";
-			timer.seconds < 10 ? clockString += "0" + timer.seconds : clockString += timer.seconds;
-		time.setAttribute("class", "timeSpent");
-		time.textContent = "Your time is " + clockString;
+		time = document.createElement('h5');
+			timer.hours < 10 ? clockString += '0' + timer.hours + ':' : clockString += timer.hours + ':';
+			timer.minutes < 10 ? clockString += '0' + timer.minutes + ':' : clockString += timer.minutes + ':';
+			timer.seconds < 10 ? clockString += '0' + timer.seconds : clockString += timer.seconds;
+		time.setAttribute('class', 'timeSpent');
+		time.textContent = `Your time is ${String(clockString)}`;
 
 		/*------DISPLAY MESSAGE TO THE PLAYER------*/
-		starNodes = [];
-		starNodes = document.querySelectorAll(".star");
-		msn = document.createElement("h5");
-		msn.setAttribute("class", "performanceMessage");
+		starNodes = document.querySelectorAll('.star');
+		msn = document.createElement('h5');
+		msn.setAttribute('class', 'performanceMessage');
+
 		if (starNodes.length !== 0) {
+			let qtnStars = '';
+
 			switch(starNodes.length) {
 				case 1:
-					msn.textContent = "Don\'t give up!!! You already won a star!";
+					msn.textContent = `Don't give up!!! You already won a star!`;
 					break;
 				case 2:
-					msn.textContent = "Wow!!! Almost there! You won two stars!";
+					msn.textContent = `Wow!!! Almost there! You won two stars!`;
 					break;
 				case 3:
-					msn.textContent = "Congratulations!!! Your memory and you are incredibles. You are three stars!";
+					msn.textContent = `Congratulations!!! Your memory and you are incredibles. You are three stars!`;
 					break;
 			}
+
 			/*------DISPLAY STARS------*/
-			stars = document.createElement("h2");
-			stars.setAttribute("class", "text-center achievedStarRating");
-			let qtnStars = "";
+			stars = document.createElement('h2');
+			stars.setAttribute('class', 'achievedStarRating');
+
 			for (let i = 0; i < starNodes.length; i++) {
-				qtnStars += "&#10023; ";
+				qtnStars += '&#10023; ';
 			}
+
 			stars.innerHTML = qtnStars;
 			containerGameOver.appendChild(stars);
 
 		} else {
-			msn.textContent = "Sorry!!! You didn\'t win stars!!!";
+			msn.textContent = `Sorry!!! You didn't win stars!!!`;
 		}
 
 		containerGameOver.appendChild(time);
 		containerGameOver.appendChild(msn);
-		
+
 		containers = [containerGameOver, ...containers];
-		
+
 		for(let i = 0; i < containers.length; i++){
 			fragment.appendChild(containers[i]);
 		}
-		
-		node.appendChild(fragment);
 
-		$("#gameOver").modal();
+		node.appendChild(fragment);
+		document.getElementById('game-over').classList.replace('modal--closed', 'modal--opened');
 	}
 }
 
 /**
 * @description This object is responsable for existence of the game.
-* @property {array} cards - The content to be shown when the user clicks on anyone.
-* @property {array} rearrangedCards - Keep the cards at a new layout.
-* @property {array} clickedCards - Keep the card that the user clicks on. Max: two cards.
-* @property {array} matched - When two cards match, they are keeped inside this array. When the array length is 28, the user wins the game.
+* @property { array } cards The content to be shown when the user clicks on any card.
+* @property { array } rearrangedCards Keep the cards at a new layout.
+* @property { array } clickedCards Keep the card that the user clicks on. Max: two cards.
+* @property { array } matched When two cards match, they are keeped inside this array. When the array length is 28, the 
+*	user wins the game.
 */
+
 let card = {
 	cards: [
-		{card: "❦", id: 0},
-		{card: "❧", id: 1},
-		{card: "☙", id: 2},
-		{card: "❥", id: 3},
-		{card: "✧", id: 4},
-		{card: "➳", id: 5},
-		{card: "💝", id: 6},
-		{card: "💕", id: 7},
-		{card: "🌹", id: 8},
-		{card: "🌷", id: 9},
-		{card: "💐", id: 10},
-		{card: "🌺", id: 11},
-		{card: "✯", id: 12},
-		{card: "❀", id: 13}],
+		{ card: "❦",		id: 0		},
+		{ card: "❧",		id: 1		},
+		{ card: "☙",		id: 2		},
+		{ card: "❥",		id: 3		},
+		{ card: "✧",		id: 4		},
+		{ card: "➳",		id: 5		},
+		{ card: "💝",		id: 6		},
+		{ card: "💕",		id: 7		},
+		{ card: "🌹",		id: 8		},
+		{ card: "🌷",		id: 9		},
+		{ card: "💐",		id: 10	},
+		{ card: "🌺",		id: 11	},
+		{ card: "✯",		id: 12	},
+		{ card: "❀",		id: 13	}
+	],
 	rearrangedCards: [],
 	clickedCards: [],
 	matched: [],
+
 	/**
-	* @description Generate random numbers between 0 and 27 (including the both numbers)
-	* @returns {array} numbers at a new position.
+	* @description Generate random numbers between 0 and 27 (including the two numbers).
+	* @returns { array } numbers at a new position.
 	*/
-	generateRandomNumbers(){
+	generateRandomNumbers() {
 		let cards = [];
+		let nCard = 0;
+		let obj = 0;
+
 		for (let i = 0; i <= 27; i++) {
 			cards[i] = i;
 		}
+
 		for (let i = cards.length; i;) {
-			let nCard = Math.random() * i-- | 0;
-			let obj = cards[nCard];
+			nCard = Math.random() * i-- | 0;
+			obj = cards[nCard];
 			cards[nCard] = cards[i];
 			cards[i] = obj;		
 		}
+
 		return cards;
 	},
+
 	/**
 	* @description With the random numbers, this method rearrange the cards.
 	*/
-	rearrangeCards(){
-		this.rearrangedCards = [];
+	rearrangeCards() {
 		let newLayout = this.generateRandomNumbers();
 		let duplicate = [...this.cards, ...this.cards];
+
+		this.rearrangedCards = [];
+
 		for (let i = 0; i <= (newLayout.length - 1); i++) {
 			this.rearrangedCards.push(duplicate[newLayout[i]]);
 		}
@@ -299,20 +318,21 @@ let card = {
 };
 
 /**
-* @description This function is responsable to create the game arena, when are all cards.
-* Use a 'document fragment' to prevent reflow and new design generation.
+* @description This function is responsable for creating the 'game arena': the place where all cards are. This function uses 
+* a 'document fragment' to prevent reflow and new design generation.
 */
-function gameArena(){
-	const node = document.getElementById("game-arena");
+function gameArena() {
 	let fragment = document.createDocumentFragment();
 	let card;
-	for (let i = 0; i <= 27; i++){
-		card = document.createElement("div");
-		card.setAttribute("id", "rr"+i);
-		card.classList.add("col-3", "card", "text-center");
-		card.textContent = ".. ? ..";
+
+	for (let i = 0; i <= 27; i++) {
+		card = document.createElement('div');
+		card.setAttribute('id', `rr${String(i)}`);
+		card.classList.add('card');
+		card.textContent = '.. ? ..';
 		
 		fragment.appendChild(card);
 	}
-	node.appendChild(fragment);
+
+	document.getElementById('game-arena').appendChild(fragment);
 }
--- asset/js/app.js
+++ asset/js/app.js
@@ -1,29 +1,49 @@
-$("#startGame").modal();
+document.getElementById('start-game').classList.replace('modal--closed', 'modal--opened');
 
 /**
 * @description This function is called in the first time after the page is loaded, when the user clicks on 
-*start game button on a modal.
+* start game button on a modal.
 */
-function startGame(){
+
+function startGame() {
+	/**
+	 * @description Close 'start-game' modal.
+	*/
+	document.getElementById('start-game').classList.replace('modal--opened', 'modal--closed');
+
 	/*------DISTRIBUTING CARDS------*/
 	card.rearrangeCards();
 
 	/*------INITIALIZING THE TIMER------*/
 	timer.timeOn();
 
-	/*------GETTING THE CONTAINER------*/
-	let container = document.querySelector("#game-arena");
+	addEventListener();
+}
 
-	$(container).on('click', 'DIV', turnCard);
+function addEventListener() {
+	const cards = document.querySelectorAll('.card');
+
+	for (let i = 0; i < cards.length; i++) {
+		cards[i].addEventListener('click', turnCard);
+	}
+}
+
+function removeEventListener() {
+	const cards = document.querySelectorAll('.card');
+
+	for (let i = 0; i < cards.length; i++) {
+		cards[i].removeEventListener('click', turnCard);
+	}
 }
 
 /**
-* @description This function is responsable to implement the rules of the game. Recognize the target of an event, apply methods 
-*and rules and prevent some mistakes during the game.
-*As soon as, verify if the user wins the game.
-* @param {event} event - Characteristics of the event.
+* @description This function is responsable for implementing the rules of the game. Recognize the target of an event, 
+* apply methods and rules and prevent some mistakes during the game. As soon as, verify if the user wins the game.
+*
+* @param { event } event Characteristics of the event.
 */
-function turnCard(event){
+
+function turnCard(event) {
 	/*------GETTING THE CURRENT ID------*/
 	let indice = event.target.id;
 
@@ -41,35 +61,40 @@ function turnCard(event){
 		}
 	}
 
-		/*------INCREASE NUMBER OF CLICKS------*/
+	/*------INCREASE NUMBER OF CLICKS------*/
 	game.click++;
 
 	/*------PREVENTS THE USER'S THIRD CLICK WHILE A setTimeout FUNCTION DOES NOT END------*/
 	if (game.click > 2) {
 		return;
 	}
-	
+
 	/*------INCREASE THE MOVE COUNTER------*/
 	moveCounter.increaseCounter();
 
 	/*------ADD STYLE CLASS------*/
-	this.classList.add("card-clicked");
+	this.classList.add('card-clicked');
 
 	for (let i = 0; i <= 27; i++) {
-		if (indice === "rr"+i){
+		if (indice === `rr${String(i)}`) {
+
 			/*------DISPLAY THE CARD------*/
 			this.textContent = card.rearrangedCards[i].card;
+
 			/*------kEEP THE CARD------*/
 			let objCard = {
 				idDoc: indice,
 				card: card.rearrangedCards[i].card,
 				idCard: card.rearrangedCards[i].id
 			};
+
 			card.clickedCards.push(objCard);
 		}
 	}
-	if (card.clickedCards.length === 2){
-				/*------CLICKED CARDS MATCH?------*/
+
+	if (card.clickedCards.length === 2) {
+
+		/*------CLICKED CARDS MATCH?------*/
 		if (card.clickedCards[0].idCard === card.clickedCards[1].idCard) {
 
 			/*------KEEP THE CARDS------*/
@@ -77,10 +102,10 @@ function turnCard(event){
 			card.matched.push(card.clickedCards[1]);
 
 			/*------STYLE CARD------*/
-			document.querySelector("#" + card.clickedCards[0].idDoc).classList.add("card-match");
-			document.querySelector("#" + card.clickedCards[1].idDoc).classList.add("card-match");
-			document.querySelector("#" + card.clickedCards[0].idDoc).classList.remove("card-clicked");
-			document.querySelector("#" + card.clickedCards[1].idDoc).classList.remove("card-clicked");
+			document.querySelector(`#${String(card.clickedCards[0].idDoc)}`).classList.add('card-match');
+			document.querySelector(`#${String(card.clickedCards[1].idDoc)}`).classList.add('card-match');
+			document.querySelector(`#${String(card.clickedCards[0].idDoc)}`).classList.remove('card-clicked');
+			document.querySelector(`#${String(card.clickedCards[1].idDoc)}`).classList.remove('card-clicked');
 
 			/*------CLEAR THE CLICKED CARDS ARRAY------*/
 			card.clickedCards = [];
@@ -90,30 +115,33 @@ function turnCard(event){
 
 			/*------VERIFY IF ALL MATCHS WERE FOUND------*/
 			if (card.matched.length === 28) {
+				removeEventListener();
 				game.gameOver();
 			}
 		} else {
 			let show;
-			
+
 			/*------DISABLE RESTART BUTTON------*/
-			document.querySelector(".buttonRestart-button").disabled = true;
-			
+			document.querySelector('.container-button-restart-the-game__button-restart-the-game').disabled = true;
+
 			/*------SHOW CLICKED CARDS WITH SOME TIME AS DELAY------*/
 			show = setTimeout(delayDisplayCard, 1000);
 		}
 	}
 }
- /**
- * @description This function is called when the user doesn't get match two cards.
- *This function is responsable to prepared the game arena for a new attempt.
- */
-function delayDisplayCard(){
-	
+
+/**
+* @description This function is called when the user doesn't get match two cards. This function is responsable for 
+* preparing the game arena for a new attempt.
+*/
+
+function delayDisplayCard() {
+
 	/*------RESTART STYLE CARD------*/
-	document.querySelector("#" + card.clickedCards[0].idDoc).textContent = ".. ? ..";
-	document.querySelector("#" + card.clickedCards[1].idDoc).textContent = ".. ? ..";
-	document.querySelector("#" + card.clickedCards[0].idDoc).classList.remove("card-clicked");
-	document.querySelector("#" + card.clickedCards[1].idDoc).classList.remove("card-clicked");
+	document.querySelector(`#${String(card.clickedCards[0].idDoc)}`).textContent = '.. ? ..';
+	document.querySelector(`#${String(card.clickedCards[1].idDoc)}`).textContent = '.. ? ..';
+	document.querySelector(`#${String(card.clickedCards[0].idDoc)}`).classList.remove('card-clicked');
+	document.querySelector(`#${String(card.clickedCards[1].idDoc)}`).classList.remove('card-clicked');
 
 	/*------CLEAR THE CLICKED ARRAY------*/
 	card.clickedCards = [];
@@ -123,21 +151,20 @@ function delayDisplayCard(){
 
 	/*------END OF TIME OUT------*/
 	clearTimeout(this.show);
-	
+
 	/*------ENABLE RESTART BUTTON------*/
-	document.querySelector(".buttonRestart-button").disabled = false;
+	document.querySelector('.container-button-restart-the-game__button-restart-the-game').disabled = false;
 }
 
 /**
-* @description This function is responsable to restart the game when the game ends and the user wants to play again and all the 
-*times that the user click on the restart game button.
-*This function calls some methods to restart the game and prepared the game arena.
-*As well as, the game controllers.
+* @description This function is responsable for restarting the game when the game ends and the user wants to play again 
+*	and all the times that the user click on the restart game button. This function calls some methods to restart the game 
+* and prepared the game arena. As well as, the game controllers.
 */
-function restartGame(){
-	/*------DISABLE THE EVENT------*/
-	let container = document.querySelector("#game-arena");
-	$(container).off('click', 'DIV');
+
+function restartGame() {
+
+	removeEventListener();
 
 	/*------CHANGE THE GAME PROPERTIES------*/
 	game.click = 0;
@@ -147,27 +174,29 @@ function restartGame(){
 
 	/*------IF THE USER CLICK ON THE BUTTON WITH OUT MATCH NO CARDS------*/
 	let cardMatrix = [];
-	cardMatrix = document.querySelectorAll(".card-match");
+	cardMatrix = document.querySelectorAll('.card-match');
 
 	if (!(cardMatrix === null)) {
 		if (cardMatrix.length === 28) {
+
 			/*------ADD ONE TO QUANTITY OF REPLAYS OF THE GAME------*/
 			game.play++;
 		}
+
 		/*------REMOVE CARD-MATCH CLASS------*/
 		for (let i = 0; i < cardMatrix.length; i++) {
-			cardMatrix[i].textContent = ".. ? ..";
-			cardMatrix[i].classList.remove("card-match");
+			cardMatrix[i].textContent = '.. ? ..';
+			cardMatrix[i].classList.remove('card-match');
 		}
 	}
 
 	/*------FIX A BUG: CLICK ON ONE CARD AND PRESS RESTART GAME BUTTON------*/
 	let cardClicked;
-	cardClicked = document.querySelector(".card-clicked");
-	
+	cardClicked = document.querySelector('.card-clicked');
+
 	if (!((cardClicked === undefined) || (cardClicked === null))) {
-		cardClicked.textContent = ".. ? ..";
-		cardClicked.classList.remove("card-clicked");
+		cardClicked.textContent = '.. ? ..';
+		cardClicked.classList.remove('card-clicked');
 	}
 
 	/*------RESET THE CARD PROPERTIES------*/
@@ -183,4 +212,13 @@ function restartGame(){
 
 	/*------CALL THE FUNCTION TO START THE GAME------*/
 	startGame();
+}
+
+function restartGameFromGameOverModal() {
+	closeGameOverModal();
+	restartGame();
+}
+
+function closeGameOverModal() {
+	document.getElementById('game-over').classList.replace('modal--opened', 'modal--closed');
 }
